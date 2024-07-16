749a1e477c0424e2d5fd83f16e653db0b8558b73
Misha Chaplin <mchaplin1@gmail.com>
2024-07-16 21:44:33 +0300

    Naming and delete function edit

--- scripts/index.js
+++ scripts/index.js
@@ -4,33 +4,34 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 
-const places = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function addCard(cardData, removeCard) {
+function createCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
   const deleteButton = cardElement.querySelector('.card__delete-button');
 
   cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
 
-  deleteButton.addEventListener('click', removeCard);
+  const card = cardElement;
+  deleteButton.addEventListener('click', () => deleteCard(card));
 
   return cardElement;
 };
 
 // @todo: Функция удаления карточки
 
-function removeCard(evt) {
-  const card = evt.target.closest('.card');
-  card.remove();
+function deleteCard(card) {
+  card.remove()
 };
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function (elem) {
-  places.append(addCard(elem, removeCard));
+  cardsContainer.append(createCard(elem, deleteCard));
 });
